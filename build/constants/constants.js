c0d004f25268d996cc422bfa159d063e6eb31c61
definarsociety <defnar@proton.me>
2025-12-12 16:59:44 +0530

    removed build folder

--- build/constants/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_VERSION = exports.APP_NAME = void 0;
-exports.APP_NAME = "Test REST API";
-exports.API_VERSION = "1.0.0";
